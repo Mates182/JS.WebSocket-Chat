06f7a6952494b2ca3be96233f6a5ceea5ba3ce10
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2024-12-01 17:22:25 -0500

    feat: :sparkles: Update index.js

--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@ import express from 'express'
 const port = process.env.PORT ?? 3000
 const app = express()
 
-app.getMaxListeners('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send('<h1>This is the chat</h1>')
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
