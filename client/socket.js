afd4430dd5fd76d17acd7104994dd101cba60290
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2024-12-01 18:36:25 -0500

    feat: :sparkles: Broadcast the message by the socket

--- client/socket.js
+++ client/socket.js
@@ -3,6 +3,12 @@ const socket = io()
 
 const form = document.getElementById('form')
 const input = document.getElementById('input')
+const messages = document.getElementById('messages')
+
+socket.on('chat message', (msg) =>{
+    const item = `<li>${msg}</li>`
+    messages.insertAdjacentHTML('beforeend', item)
+})
 
 form.addEventListener('submit', (e) =>{
     e.preventDefault()
